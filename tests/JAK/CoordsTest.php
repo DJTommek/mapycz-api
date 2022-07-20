4bd967fd490f898861ec49802c8aea3de2fa73cf
Tomas Palider (DJTommek) <tomas@palider.cz>
2022-07-21 00:36:26 +0200

    Utils: added Javascript to PHP transcript for JAK.Coords.coordsToString() and JAK.Coords._serializeNumber() - added tests

--- tests/JAK/CoordsTest.php
+++ tests/JAK/CoordsTest.php
@@ -17,9 +17,26 @@ final class CoordsTest extends TestCase
 		], Coords::stringToCoords('9fJgGxW.HqkQ0xWn3F9fWDGxX0wGlQ0xW9oq'));
 	}
 
+	public function testCoordsToString(): void
+	{
+		$this->assertSame('9hd0GxXt2G', Coords::coordsToString([[14.61182713508606, 50.07315695285797]]));
+		$this->assertSame('9fJgGxW.HqkQ0xWn3F9fWDGxX0wGlQ0xW9oq', Coords::coordsToString([
+			[13.780299425125122, 49.91954952478409],
+			[13.809481859207153, 49.87952224910259],
+			[13.859606981277466, 49.92441236972809],
+			[13.894282579421997, 49.90208297967911],
+		]));
+	}
+
 	public function testParseNumber(): void
 	{
 		$a = ['G', '2', 't', 'X', 'x'];
 		$this->assertSame(15846853, Coords::_parseNumber($a, 4));
+	}
+
+	public function testSerializeNumber(): void
+	{
+		$this->assertSame('9hd0G', Coords::_serializeNumber(145113096, 145113096));
+		$this->assertSame('xWn3F', Coords::_serializeNumber(-59693, 208603463));
 	}
 }
