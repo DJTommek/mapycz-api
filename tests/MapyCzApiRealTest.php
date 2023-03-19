535681690a96a8b317b48e5dac527c9b8da55bfb
Tomas Palider (DJTommek) <tomas@palider.cz>
2023-03-19 09:34:38 +0100

    Tests: updated real-requests tests to be more resilient against external API data change

--- tests/MapyCzApiRealTest.php
+++ tests/MapyCzApiRealTest.php
@@ -17,8 +17,16 @@ final class MapyCzApiRealTest extends TestCase
 	{
 		$latReal = $object->getLat();
 		$lonReal = $object->getLon();
-		$this->assertEqualsWithDelta($latExpected, $latReal, 0.00001, sprintf('Failed asserting that latitude %s matches expected %s', $latReal, $latExpected));
-		$this->assertEqualsWithDelta($lonExpected, $lonReal, 0.00001, sprintf('Failed asserting that longitude %s matches expected %s', $lonReal, $lonExpected));
+		// Very low coordinate precision due to often changes on API server
+		$this->assertEqualsWithDelta($latExpected, $latReal, 0.001, sprintf('Failed asserting that latitude %s matches expected %s', $latReal, $latExpected));
+		$this->assertEqualsWithDelta($lonExpected, $lonReal, 0.001, sprintf('Failed asserting that longitude %s matches expected %s', $lonReal, $lonExpected));
+	}
+
+	private function assertNotEmptyString(mixed $input): void
+	{
+		$this->assertTrue(is_string($input));
+		$this->assertTrue(trim($input) !== '');
+		assert(is_string($input));
 	}
 
 	/**
@@ -138,129 +146,35 @@ final class MapyCzApiRealTest extends TestCase
 	public function testLoadReverseGeocode(): void
 	{
 		$data = $this->api->reverseGeocode(50.088024, 14.421580);
-		$this->assertSame('Staroměstské náměstí 606/11, Praha, 110 00, Hlavní město Praha', $data->label);
+		$address = $data->getAddress();
+		$this->assertNotEmptyString($address);
+		$this->assertStringContainsString('Staroměstské náměstí', $address);
+		$this->assertStringContainsString('Praha', $address);
 		$this->assertSame('Ok', $data->message);
 		$this->assertSame(200, $data->status);
-		$this->assertSame('Staroměstské náměstí 606/11, Praha, 110 00, Hlavní město Praha', $data->getAddress());
-		$this->assertSame(50.08801489569467, $data->getLat());
-		$this->assertSame(14.421563306025112, $data->getLon());
+		$this->assertCoordsDelta(50.08801489569467, 14.421563306025112, $data);
 
-		$this->assertCount(8, $data->items);
+		$this->assertTrue(count($data->items) > 5);
 
-		$this->assertSame(8939832, $data->items[0]->id);
-		$this->assertSame('Staroměstské náměstí 606/11', $data->items[0]->name);
-		$this->assertSame('addr', $data->items[0]->source);
-		$this->assertSame('addr', $data->items[0]->type);
-		$this->assertSame(14.421563306025112, $data->items[0]->x);
-		$this->assertSame(50.08801489569467, $data->items[0]->y);
-		$this->assertSame(50.08801489569467, $data->items[0]->getLat());
-		$this->assertSame(14.421563306025112, $data->items[0]->getLon());
+		$matchedCount = 0;
 
-		$this->assertSame(121933, $data->items[1]->id);
-		$this->assertSame('Staroměstské náměstí', $data->items[1]->name);
-		$this->assertSame('stre', $data->items[1]->source);
-		$this->assertSame('stre', $data->items[1]->type);
-		$this->assertSame(14.420509314368898, $data->items[1]->x);
-		$this->assertSame(50.08777079179988, $data->items[1]->y);
-		$this->assertSame(50.08777079179988, $data->items[1]->getLat());
-		$this->assertSame(14.420509314368898, $data->items[1]->getLon());
+		foreach ($data->items as $item) {
+			if ($item->type === 'coun') {
+				$this->assertSame('Česko', $item->name);
+				$matchedCount++;
+			} else if ($item->type === 'muni') {
+				$this->assertSame('Praha', $item->name);
+				$matchedCount++;
+			} else if ($item->type === 'stre') {
+				$this->assertSame('Staroměstské náměstí', $item->name);
+				$matchedCount++;
+			} else if ($item->type === 'addr') {
+				$this->assertStringContainsString('Staroměstské náměstí', $item->name);
+				$matchedCount++;
+			}
 
-		$this->assertSame(87, $data->items[2]->id);
-		$this->assertSame('Praha 1', $data->items[2]->name);
-		$this->assertSame('quar', $data->items[2]->source);
-		$this->assertSame('quar', $data->items[2]->type);
-		$this->assertSame(14.424132200124081, $data->items[2]->x);
-		$this->assertSame(50.08783679317715, $data->items[2]->y);
-		$this->assertSame(50.08783679317715, $data->items[2]->getLat());
-		$this->assertSame(14.424132200124081, $data->items[2]->getLon());
-
-		$this->assertSame(13674, $data->items[3]->id);
-		$this->assertSame('Staré Město', $data->items[3]->name);
-		$this->assertSame('ward', $data->items[3]->source);
-		$this->assertSame('ward', $data->items[3]->type);
-		$this->assertSame(14.417781898902357, $data->items[3]->x);
-		$this->assertSame(50.084551750804636, $data->items[3]->y);
-		$this->assertSame(50.084551750804636, $data->items[3]->getLat());
-		$this->assertSame(14.417781898902357, $data->items[3]->getLon());
-
-		$this->assertSame(3468, $data->items[4]->id);
-		$this->assertSame('Praha', $data->items[4]->name);
-		$this->assertSame('muni', $data->items[4]->source);
-		$this->assertSame('muni', $data->items[4]->type);
-		$this->assertSame(14.4341412988, $data->items[4]->x);
-		$this->assertSame(50.0835493857, $data->items[4]->y);
-		$this->assertSame(50.0835493857, $data->items[4]->getLat());
-		$this->assertSame(14.4341412988, $data->items[4]->getLon());
-
-		$this->assertSame(47, $data->items[5]->id);
-		$this->assertSame('Okres Hlavní město Praha', $data->items[5]->name);
-		$this->assertSame('dist', $data->items[5]->source);
-		$this->assertSame('dist', $data->items[5]->type);
-		$this->assertSame(14.466000012808934, $data->items[5]->x);
-		$this->assertSame(50.066789200117995, $data->items[5]->y);
-		$this->assertSame(50.066789200117995, $data->items[5]->getLat());
-		$this->assertSame(14.466000012808934, $data->items[5]->getLon());
-
-		$this->assertSame(10, $data->items[6]->id);
-		$this->assertSame('Hlavní město Praha', $data->items[6]->name);
-		$this->assertSame('regi', $data->items[6]->source);
-		$this->assertSame('regi', $data->items[6]->type);
-		$this->assertSame(14.466, $data->items[6]->x);
-		$this->assertSame(50.066789, $data->items[6]->y);
-		$this->assertSame(50.066789, $data->items[6]->getLat());
-		$this->assertSame(14.466, $data->items[6]->getLon());
-
-		$this->assertSame(112, $data->items[7]->id);
-		$this->assertSame('Česko', $data->items[7]->name);
-		$this->assertSame('coun', $data->items[7]->source);
-		$this->assertSame('coun', $data->items[7]->type);
-		$this->assertSame(15.338411, $data->items[7]->x);
-		$this->assertSame(49.742858, $data->items[7]->y);
-		$this->assertSame(49.742858, $data->items[7]->getLat());
-		$this->assertSame(15.338411, $data->items[7]->getLon());
-
-
-		$data = $this->api->reverseGeocode(50.133923, 14.409660);
-		$this->assertSame('Dolákova, Praha, Hlavní město Praha', $data->getAddress());
-		$this->assertSame('Ok', $data->message);
-		$this->assertSame(200, $data->status);
-		$this->assertSame('Dolákova, Praha, Hlavní město Praha', $data->getAddress());
-		$this->assertSame(50.13358200394987, $data->getLat());
-		$this->assertSame(14.406162964024444, $data->getLon());
-		$this->assertCount(7, $data->items);
-
-
-		$data = $this->api->reverseGeocode(40.414711, -3.700830);
-		$this->assertSame('Plaza de Santa Ana', $data->getAddress());
-		$this->assertSame('Ok', $data->message);
-		$this->assertSame(200, $data->status);
-		$this->assertSame('Plaza de Santa Ana', $data->getAddress());
-		$this->assertSame(40.41480397411152, $data->getLat());
-		$this->assertSame(-3.700791339178318, $data->getLon());
-		$this->assertCount(7, $data->items);
-
-		$this->assertSame(1023817397, $data->items[0]->id);
-		$this->assertSame('Plaza de Santa Ana', $data->items[0]->name);
-		$this->assertSame('osm', $data->items[0]->source);
-		$this->assertSame('osms', $data->items[0]->type);
-		$this->assertSame(-3.700791339178318, $data->items[0]->x);
-		$this->assertSame(40.41480397411152, $data->items[0]->y);
-		$this->assertSame(40.41480397411152, $data->items[0]->getLat());
-		$this->assertSame(-3.700791339178318, $data->items[0]->getLon());
-
-
-		$data = $this->api->reverseGeocode(0, 0);
-		$this->assertSame('Atlantský oceán', $data->getAddress());
-		$this->assertCount(1, $data->items);
-
-		$this->assertSame(112794890, $data->items[0]->id);
-		$this->assertSame('Atlantic Ocean', $data->items[0]->name);
-		$this->assertSame('osm', $data->items[0]->source);
-		$this->assertSame('osma', $data->items[0]->type);
-		$this->assertSame(-38.32031204752161, $data->items[0]->x);
-		$this->assertSame(13.58192094506344, $data->items[0]->y);
-		$this->assertSame(13.58192094506344, $data->items[0]->getLat());
-		$this->assertSame(-38.32031204752161, $data->items[0]->getLon());
+		}
+		$this->assertSame(4, $matchedCount, sprintf('Some item are missing, matched only %d out of %d.', $matchedCount, 4));
 	}
 
 	public function testLoadReverseGeocodeError1(): void
@@ -276,15 +190,14 @@ final class MapyCzApiRealTest extends TestCase
 		$options->zoom = 13;
 		$options->mapsetId = 1;
 		$places = $this->api->loadLookupBox(14.099642, 49.997597, 14.367434, 50.102973, $options);
-		$this->assertCount(7, $places);
 
-		// @TODO results are kind of random - so just checking, that no exception is thrown.
-//		$this->assertEquals('Obchvat Jinočan - Okružní ulice', $places[0]->title);
-//		$this->assertEquals(50.0339410763, $places[0]->getLat());
-//		$this->assertEquals(14.2750335485, $places[0]->getLon());
-//		$this->assertEquals('Letiště Václava Havla Praha (PRG)', $places[3]->title);
-//		$this->assertEquals(50.1083951973, $places[3]->getLat());
-//		$this->assertEquals(14.2621233398, $places[3]->getLon());
+		// Results are kind of random - so this is just general check
+		$this->assertTrue(count($places) > 0);
+		foreach ($places as $place) {
+			$this->assertNotEmptyString($place->title);
+			$this->assertIsFloat($place->getLat());
+			$this->assertIsFloat($place->getLon());
+		}
 	}
 
 	public function testLoadLookupBoxError1(): void
@@ -298,14 +211,21 @@ final class MapyCzApiRealTest extends TestCase
 	public function testLoadPanoramaGetBest(): void
 	{
 		$bestPanorama = $this->api->loadPanoramaGetBest(14.421127, 50.087726);
-		$this->assertSame(70102895, $bestPanorama->pid);
-		$this->assertEqualsWithDelta(255.76090162311178, $bestPanorama->azimuth, 0.0001);
-		$this->assertEqualsWithDelta(27.56126704628921, $bestPanorama->distance, 0.0001);
-		$this->assertEqualsWithDelta(255.76090162311178, $bestPanorama->lookDir, 0.0001);
+		$this->assertInstanceOf(\DJTommek\MapyCzApi\Types\PanoramaBestType::class, $bestPanorama);
+		$this->assertIsInt($bestPanorama->pid);
+		$this->assertIsFloat($bestPanorama->azimuth);
+		$this->assertIsFloat($bestPanorama->distance);
+		$this->assertIsFloat($bestPanorama->lookDir);
 		$this->assertSame(50, $bestPanorama->radius);
-		$this->assertSame('https://mapy.cz/zakladni?x=14.421499685654&y=50.087788829892&pano=1&pid=70102895&yaw=4.4638698311926', $bestPanorama->getLink());
+		$link = $bestPanorama->getLink();
+		$this->assertNotEmptyString($link);
+		$this->assertStringStartsWith('https://mapy.cz/zakladni?x=', $link);
+		$this->assertStringContainsString('&pano=1&', $link);
+	}
 
-		// same panorama but limited radius
-		$this->assertNull($this->api->loadPanoramaGetBest(14.421127, 50.087726, 10));
+	public function testLoadPanoramaGetBestNotFound(): void
+	{
+		$bestPanorama = $this->api->loadPanoramaGetBest(14.421127, 50.087726, 10);
+		$this->assertNull($bestPanorama);
 	}
 }
